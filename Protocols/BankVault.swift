f87f391155c8034d56da6e8c97ac2d1798141baf
Dougly <dgalante11@gmail.com>
2016-10-20 10:19:58 -0400

    lab complete

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -20,7 +20,27 @@ class BankVault {
     
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword (_ password: [Int]) -> Bool
+}
 
 
-
-
+extension BankVault: ProvideAccess {
+    
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        
+        if password.isEmpty || password.count > 10 {
+            return false
+        }
+        
+        for (index, digit) in password.enumerated() {
+            if index % 2 == 0 {
+                if digit % 2 != 0 {
+                    return false
+                }
+            }
+        }
+        
+        return true
+    }
+}
